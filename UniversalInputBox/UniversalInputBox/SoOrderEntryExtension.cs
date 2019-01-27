dff446ac09a0ef3f7259eb129699d8569d94e670
Maksimilian Pekarets <m.pekarets@itransition.com>
2019-01-27 05:50:59 +0300

    Fixed inserting new sales order. Created input parsing prototype

--- UniversalInputBox/UniversalInputBox/SoOrderEntryExtension.cs
+++ UniversalInputBox/UniversalInputBox/SoOrderEntryExtension.cs
@@ -1,10 +1,21 @@
-﻿using PX.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PX.Data;
+using PX.Objects.AR;
+using PX.Objects.Common.Abstractions;
 using PX.Objects.SO;
 
 namespace UniversalInputBox
 {
     public class SoOrderEntryExtension : PXGraphExtension<SOOrderEntry>
     {
+        private static readonly IEnumerable<string> Delimiters = new List<string>
+        {
+            " ",
+            Environment.NewLine
+        };
+
         public PXFilter<SalesOrderFilter> SearchRequestFilter;
 
         public PXAction<SOOrder> search;
@@ -13,15 +24,58 @@ namespace UniversalInputBox
         [PXUIField(DisplayName = "Go")]
         public virtual void Search()
         {
-            var searchRequest = SearchRequestFilter.Current.SearchRequest;
-            CreateSalesOrderPrototype(searchRequest);
+            var salesOrder = Base.Document.Current;
+            if (IsExistingOrderSelected(salesOrder))
+            {
+                Base.Document.Current = null;
+                salesOrder = Base.Document.Insert();
+            }
+            var salesOrderModel = GetSalesOrderModel();
+            FillSalesOrder(salesOrderModel, salesOrder);
+            Base.Document.Current = salesOrder;
         }
 
-        private void CreateSalesOrderPrototype(string searchRequest)
+        private void FillSalesOrder(SalesOrderModel salesOrderModel, SOOrder salesOrder)
         {
-            var salesOrder = Base.Document.Insert();
-            salesOrder.OrderDesc = searchRequest;
-            Base.Document.Current = salesOrder;
+            salesOrder.OrderDesc = "Team Alpha";
+            salesOrder.CustomerID = GetCustomerId(salesOrderModel.CustomerCd);
+            salesOrder.RequestDate = GetRequestDate(salesOrderModel.RequestDate);
+        }
+
+        private bool IsExistingOrderSelected(IDocumentKey salesOrder)
+        {
+            return Base.Document.Cache.GetStatus(salesOrder) != PXEntryStatus.Inserted;
+        }
+
+        private int? GetCustomerId(string customerCd)
+        {
+            var customer = new PXSelect<Customer,
+                Where<Customer.acctCD, Equal<Required<Customer.acctCD>>>>(Base).SelectSingle(customerCd);
+            return customer.BAccountID;
+        }
+
+        private DateTime? GetRequestDate(string requestedOn)
+        {
+            switch (requestedOn)
+            {
+                case "today":
+                    return Base.Accessinfo.BusinessDate;
+                case "tomorrow":
+                    return Base.Accessinfo.BusinessDate.GetValueOrDefault().AddDays(1);
+                default:
+                    return DateTime.Parse(requestedOn);
+            }
+        }
+
+        private SalesOrderModel GetSalesOrderModel()
+        {
+            var request = SearchRequestFilter.Current.SearchRequest;
+            var fields = request.Split(Delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+            return new SalesOrderModel
+            {
+                CustomerCd = fields[0],
+                RequestDate = fields[1]
+            };
         }
     }
 }
