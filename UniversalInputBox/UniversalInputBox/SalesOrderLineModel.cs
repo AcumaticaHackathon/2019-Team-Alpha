8597869e3a5be2578035d9c7cb855b28ad073429
Maksimilian Pekarets <46930658+m-pekarets@users.noreply.github.com>
2019-01-27 05:54:03 +0300

    Fixed inserting new sales order. Created input parsing prototype (#1)

---
+++ UniversalInputBox/UniversalInputBox/SalesOrderLineModel.cs
@@ -0,0 +1,17 @@
+﻿namespace UniversalInputBox
+{
+    public class SalesOrderLineModel
+    {
+        public string InventoryItemId
+        {
+            get;
+            set;
+        }
+
+        public string QuantityAndUnitPrice
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ UniversalInputBox/UniversalInputBox/SalesOrderModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace UniversalInputBox
+{
+    public class SalesOrderModel
+    {
+        public string CustomerCd
+        {
+            get;
+            set;
+        }
+
+        public string RequestDate
+        {
+            get;
+            set;
+        }
+
+        public string ShipVia
+        {
+            get;
+            set;
+        }
+
+        public IEnumerable<SalesOrderLineModel> LineItems
+        {
+            get;
+            set;
+        }
+    }
+}
