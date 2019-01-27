ca4d5f970fc9dccd7b9e57a0fd5b9760fa5451a0
Maksimilian Pekarets <m.pekarets@itransition.com>
2019-01-27 03:33:38 +0300

    Solution is created. Sales order filter is added

---
+++ UniversalInputBox/UniversalInputBox/SalesOrderFilter.cs
@@ -0,0 +1,15 @@
+﻿using PX.Data;
+
+namespace UniversalInputBox
+{
+    public class SalesOrderFilter : IBqlTable
+    {
+        [PXString]
+        [PXUIField(DisplayName = "Search")]
+        public virtual string SearchRequest
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ UniversalInputBox/UniversalInputBox/SoOrderEntryExtension.cs
@@ -0,0 +1,27 @@
+﻿using PX.Data;
+using PX.Objects.SO;
+
+namespace UniversalInputBox
+{
+    public class SoOrderEntryExtension : PXGraphExtension<SOOrderEntry>
+    {
+        public PXFilter<SalesOrderFilter> SearchRequestFilter;
+
+        public PXAction<SOOrder> search;
+
+        [PXButton(CommitChanges = true)]
+        [PXUIField(DisplayName = "Go")]
+        public virtual void Search()
+        {
+            var searchRequest = SearchRequestFilter.Current.SearchRequest;
+            CreateSalesOrderPrototype(searchRequest);
+        }
+
+        private void CreateSalesOrderPrototype(string searchRequest)
+        {
+            var salesOrder = Base.Document.Insert();
+            salesOrder.OrderDesc = searchRequest;
+            Base.Document.Current = salesOrder;
+        }
+    }
+}
